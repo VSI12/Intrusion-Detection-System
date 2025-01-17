37d7411b9842752d3411eff1bc5751ac75d63633
Victor Iliya <VictorIliya15@gmail.com>
2025-01-17 06:38:03 +0100

    Loading my S3 bucket and region from my .env

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,12 +1,20 @@
+import os
 import boto3
 
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
 from botocore.exceptions import ClientError
 
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, methods=["POST"])
+
+# Fetch environment variables
+load_dotenv()  
+AWS_REGION = os.getenv("AWS_REGION")
+S3_BUCKET = os.getenv("S3_BUCKET_NAME")
+
 @app.route('/')
 def home():
     return "Welcome to the Intrusion Detection System!"
