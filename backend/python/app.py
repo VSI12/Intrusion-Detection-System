cd472fa5c81002602e5161c3010cd88fa7476a4a
Victor Iliya <VictorIliya15@gmail.com>
2025-01-17 06:30:57 +0100

    Installed the boto3 library to use AWS services

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,5 +1,9 @@
+import boto3
+
 from flask import Flask
 from flask_cors import CORS
+from botocore.exceptions import ClientError
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, methods=["POST"])
