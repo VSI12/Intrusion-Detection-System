a35a7a847b774c8d81e7dfad6b8df866b63f6e11
Victor Iliya <VictorIliya15@gmail.com>
2025-01-17 06:14:11 +0100

    Changed the styling a bit

--- frontend/src/components/models.tsx
+++ frontend/src/components/models.tsx
@@ -23,13 +23,13 @@ const ModelSelectionForm: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h2 className="text-2xl font-semibold mb-6 text-gray-800">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-2xl font-semibold mb-6 text-white-800">
         Select a Model for Classification
       </h2>
       <button
         onClick={handleModelSelect}
-        className="px-6 py-3 bg-blue-500 text-white font-medium rounded shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        className="px-6 py-3 bg-black-500 text-white font-medium rounded shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
       >
         Commence
       </button>
