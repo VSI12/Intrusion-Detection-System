8c5e4bbd4dc765a7ddb1447af072f529ef9d446d
Victor Iliya <VictorIliya15@gmail.com>
2025-01-17 06:46:50 +0100

    Created the backend route to generate the S3 pre-signed URL

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,7 +1,7 @@
 import os
 import boto3
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from botocore.exceptions import ClientError
@@ -15,9 +15,34 @@ load_dotenv()
 AWS_REGION = os.getenv("AWS_REGION")
 S3_BUCKET = os.getenv("S3_BUCKET_NAME")
 
-@app.route('/')
-def home():
-    return "Welcome to the Intrusion Detection System!"
+# Initialize the S3 and SQS client
+s3_client = boto3.client("s3", region_name=AWS_REGION)
+
+@app.route('/generate-presigned-url', methods=['POST'])
+def generate_presigned_url():
+    try:
+        data = request.json
+        file_name = data.get("file_name")
+        file_type = data.get("file_type")
+        
+        if not file_name or not file_type:
+            return jsonify({"error": "Missing file_name or file_type"}), 400
+        
+        # Generate the presigned URL for PUT
+        presigned_url = s3_client.generate_presigned_url(
+            'put_object',
+            Params={
+                'Bucket': S3_BUCKET,
+                'Key': file_name,
+                'ContentType': file_type
+            },
+            ExpiresIn=3600  # URL expires in 1 hour
+        )
+        return jsonify({"url": presigned_url})
+    except ClientError as e:
+        print(f"Error generating presigned URL: {str(e)}")
+        return jsonify({"error": "Failed to generate presigned URL"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
