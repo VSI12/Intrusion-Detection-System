b00e92d22706f298db9fc956ff14c3a24f2f89c2
Victor Iliya <VictorIliya15@gmail.com>
2025-03-26 12:06:03 +0100

    Defined the respective variables for the dev env

---
+++ Backend/Terraform/terraform/Environments/Development/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "S3 Bucket Name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
