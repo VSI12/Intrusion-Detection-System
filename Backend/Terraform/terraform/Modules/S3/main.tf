79341322d07c6681c1f298dfe2943ae1556779fc
Victor Iliya <VictorIliya15@gmail.com>
2025-03-26 11:40:41 +0100

    Created the s3 main.tf block

---
+++ Backend/Terraform/terraform/Modules/S3/main.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "upload_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+  
+}
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.upload_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.upload_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.upload_bucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
