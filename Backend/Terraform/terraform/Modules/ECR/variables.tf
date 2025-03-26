2411204b9dad981e3170d37bc4117d3697beee1d
Victor Iliya <VictorIliya15@gmail.com>
2025-03-26 16:34:17 +0100

    Defined my respective variables

---
+++ Backend/Terraform/terraform/Modules/ECR/variables.tf
@@ -0,0 +1,9 @@
+variable "ecr_name" {
+    description = "The name of the ECR repository"
+    type        = string
+}
+
+variable "environment" {
+    description = "Deployment environment"
+    type        = string
+}
