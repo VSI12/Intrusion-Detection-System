f342859ad3a0ada4beeec9095bdaaea0466db663
Victor Iliya <VictorIliya15@gmail.com>
2025-03-26 12:16:45 +0100

    Removed the acl, invalid configuration

--- Backend/Terraform/terraform/Modules/S3/main.tf
+++ Backend/Terraform/terraform/Modules/S3/main.tf
@@ -7,10 +7,6 @@ resource "aws_s3_bucket" "upload_bucket" {
   }
   
 }
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.upload_bucket.id
-  acl    = "private"
-}
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.upload_bucket.id
