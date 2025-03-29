c2f834aa192c15155adeb93f40aaeec7ca48fd73
Victor Iliya <VictorIliya15@gmail.com>
2025-03-29 22:28:41 +0100

    Removed Sqs

--- Terraform/terraform/Modules/S3/variables.tf
+++ Terraform/terraform/Modules/S3/variables.tf
@@ -7,18 +7,3 @@ variable "environment" {
   description = "Deployment environment"
   type        = string
 }
-
-variable "queue_arn" {
-  description = "The ARN of the SQS queue"
-  type        = string
-}
-
-variable "queue_name" {
-  description = "The name of the SQS queue"
-  type        = string
-}
-
-variable "queue_policy" {
-  description = "The policy of the SQS queue"
-  type        = string
-}
