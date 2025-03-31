ef77b7ea8a992cb7330b63096c7536265d9993c0
Victor Iliya <VictorIliya15@gmail.com>
2025-03-31 10:26:13 +0100

    Defined the respective variables

--- Terraform/terraform/Modules/VPC/variables.tf
+++ Terraform/terraform/Modules/VPC/variables.tf
@@ -12,3 +12,18 @@ variable "vpc_name" {
   description = "value of the VPC name"
   type        = string
 }
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of Availability Zones"
+  type        = list(string)
+}
