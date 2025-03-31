a141cf75b627970e5e2529c6f755246392ff0406
Victor Iliya <VictorIliya15@gmail.com>
2025-03-31 13:43:14 +0100

    Defined the variable for the ecs cluster name and environment

---
+++ Terraform/terraform/Modules/ECS/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  description = "value of the ECS cluster name"
+  type        = string
+}
+
+variable "environment" {
+  description = "value of the environment name"
+  type        = string
+}
