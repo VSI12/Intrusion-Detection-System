1b08cb94d2949ade8be921b81b6f12706b65b48e
Victor Iliya <VictorIliya15@gmail.com>
2025-03-31 10:37:12 +0100

    Defined the resective variables

--- Terraform/terraform/Environments/Development/variables.tf
+++ Terraform/terraform/Environments/Development/variables.tf
@@ -18,3 +18,24 @@ variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
 }
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of Availability Zones"
+  type        = list(string)
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
