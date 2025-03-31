b1ec09944970169480e09a8e4d3a7b51f78b84a4
Victor Iliya <VictorIliya15@gmail.com>
2025-03-31 10:36:47 +0100

    Added variable defoinitoions to the vpc module

--- Terraform/terraform/Environments/Development/main.tf
+++ Terraform/terraform/Environments/Development/main.tf
@@ -9,4 +9,8 @@ module "vpc" {
   environment = var.environment
   vpc_cidr_block = var.vpc_cidr_block
   vpc_name = var.vpc_name
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones = var.availability_zones
+
 }
