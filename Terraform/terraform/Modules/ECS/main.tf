62ed3aca1be673dd6c705bdb9158c8419f999025
Victor Iliya <VictorIliya15@gmail.com>
2025-03-31 19:51:16 +0100

    task definition for the backend

--- Terraform/terraform/Modules/ECS/main.tf
+++ Terraform/terraform/Modules/ECS/main.tf
@@ -6,3 +6,27 @@ resource "aws_ecs_cluster" "IDS_cluster" {
     environment = var.environment
   }
 }
+
+resource "aws_ecs_task_definition" "backend" {
+  family                   = "backend-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "backend-container"
+      image = aws_ecr_repository.flask_repo.repository_url
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
