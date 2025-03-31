77920895b7053f1cd9ebe53389b5315b5c524d36
Victor Iliya <VictorIliya15@gmail.com>
2025-03-31 15:06:55 +0100

    Output the repos arn

--- Terraform/terraform/Modules/ECR/outputs.tf
+++ Terraform/terraform/Modules/ECR/outputs.tf
@@ -6,3 +6,11 @@ output "nextjs_repo_url" {
   value = aws_ecr_repository.NextJS_ecr.repository_url
 }
 
+output "flask_repo_arn" {
+  value = aws_ecr_repository.flask_repo.arn 
+}
+
+output "nextjs_repo_arn" {
+  value = aws_ecr_repository.NextJS_ecr.arn 
+  
+}
