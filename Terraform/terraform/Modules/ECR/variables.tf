1bb759a55bbbb60bc708deb589826b01a02b36e2
Victor Iliya <VictorIliya15@gmail.com>
2025-03-31 13:16:04 +0100

    Defined teh variables

--- Terraform/terraform/Modules/ECR/variables.tf
+++ Terraform/terraform/Modules/ECR/variables.tf
@@ -1,4 +1,8 @@
-variable "ecr_name" {
+variable "next_ecr" {
+    description = "The name of the ECR repository"
+    type        = string
+}
+variable "flask_ecr" {
     description = "The name of the ECR repository"
     type        = string
 }
