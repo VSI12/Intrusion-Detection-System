b08d7ef26ff8c8d0addf2d5e51e47db0818073b1
Victor Iliya <VictorIliya15@gmail.com>
2025-03-31 10:25:57 +0100

    Added gthe internt gateway fpor te VPC and respective subnets

--- Terraform/terraform/Modules/VPC/main.tf
+++ Terraform/terraform/Modules/VPC/main.tf
@@ -8,3 +8,38 @@ resource "aws_vpc" "ids-vpc" {
     Environment = var.environment
   }
 }
+
+resource "aws_internet_gateway" "ids_igw" {
+  vpc_id = aws_vpc.ids-vpc.id
+
+  tags = {
+    Name        = "${var.vpc_name}-igw"
+    Environment = var.environment
+  }
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.ids-vpc.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-Public Subnet-${count.index + 1}"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.ids-vpc.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.environment}-Private Subnet-${count.index + 1}"
+  }
+}
