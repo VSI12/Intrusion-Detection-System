b4708c61b20058ccaf09a2daa8e534d126e90392
Victor Iliya <VictorIliya15@gmail.com>
2025-04-01 10:04:45 +0100

    changed the sg name and tags

--- Terraform/terraform/Modules/ALB/security-group.tf
+++ Terraform/terraform/Modules/ALB/security-group.tf
@@ -1,10 +1,10 @@
 resource "aws_security_group" "nextjs_alb_sg" {
-  name        = "alb-sg"
-  description = "Allow HTTP traffic to ALB"
+  name        = "${var.alb_name_external}-sg"
+  description = "Security group for the NextJS ALB"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 443
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
@@ -17,6 +17,7 @@ resource "aws_security_group" "nextjs_alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
+    Name        = "${var.alb_name_external}-sg"
     Environment = var.environment
   }
 }
