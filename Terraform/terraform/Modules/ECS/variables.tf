43b2e8a4aa22ec80bebb86997a86883e760fe4ee
Victor Iliya <VictorIliya15@gmail.com>
2025-04-01 12:18:03 +0100

    Declared the variblaes for the alb

--- Terraform/terraform/Modules/ECS/variables.tf
+++ Terraform/terraform/Modules/ECS/variables.tf
@@ -8,17 +8,11 @@ variable "environment" {
   type        = string
 }
 
-variable "backend_taskdefinition" {
-  description = "value of the backend task definition name"
-  type        = string
-}
-
-variable "container_def" {
-  description = "value of the container definition name"
-  type        = string
-}
-
 variable "next_ecr" {
+  description = "The name of the ECR repository"
+  type        = string
+}
+variable "next_ecr_arn" {
   description = "The name of the ECR repository"
   type        = string
 }
@@ -28,18 +22,8 @@ variable "private_subnet_ids" {
   description = "List of private subnet IDs"
 }
 
-variable "alb_tg" {
-  description = "The ARN of the ALB target group"
-  type        = string
-}
-
 variable "ecs_sg" {
   description = "The security group ID for the ECS service"
-  type        = string
-}
-
-variable "alb_listener" {
-  description = "The ALB listener ARN"
   type        = string
 }
 
@@ -55,5 +39,19 @@ variable "next_container_port" {
 
 variable "ecs_execution_role" {
   description = "The ARN of the ECS execution role"
+  type        = string
+}
+
+variable "nextjs_alb_listener_arn" {
+  description = "The ARN of the external ALB listener"
+  type        = string
+}
+
+variable "nextjs_alb_target_group_arn" {
+  description = "The ARN of the NextJS Fargate target group"
+  type        = string
+}
+variable "role_name" {
+  description = "ECS service role name"
   type        = string
 }
