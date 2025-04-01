3f9fbdb5039339d26b76e93d9fcce6eb4db24b96
Victor Iliya <VictorIliya15@gmail.com>
2025-04-01 07:50:11 +0100

    Declared the variables for ehr alb name and aenvironment

---
+++ Terraform/terraform/Modules/ALB/variables.tf
@@ -0,0 +1,9 @@
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment name"
+  type        = string
+}
