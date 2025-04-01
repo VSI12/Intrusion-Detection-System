34b6892826e5d99116f22be8f5ba0fb7a518ae9f
Victor Iliya <VictorIliya15@gmail.com>
2025-04-01 10:46:31 +0100

    declared the variables

--- Terraform/terraform/Modules/ECS/variables.tf
+++ Terraform/terraform/Modules/ECS/variables.tf
@@ -17,3 +17,43 @@ variable "container_def" {
   description = "value of the container definition name"
   type        = string
 }
+
+variable "next_ecr" {
+  description = "The name of the ECR repository"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "List of private subnet IDs"
+}
+
+variable "alb_tg" {
+  description = "The ARN of the ALB target group"
+  type        = string
+}
+
+variable "ecs_sg" {
+  description = "The security group ID for the ECS service"
+  type        = string
+}
+
+variable "alb_listener" {
+  description = "The ALB listener ARN"
+  type        = string
+}
+
+variable "nextjs_service" {
+  description = "The name of the ECS service"
+  type        = string
+}
+
+variable "next_container_port" {
+  description = "The name of the container"
+  type        = string
+}
+
+variable "ecs_execution_role" {
+  description = "The ARN of the ECS execution role"
+  type        = string
+}
