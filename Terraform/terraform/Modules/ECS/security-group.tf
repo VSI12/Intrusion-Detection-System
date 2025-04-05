f8871a156c7ccec80536e2f0a5286a64d18fc842
Victor Iliya <VictorIliya15@gmail.com>
2025-04-05 10:17:17 +0100

    Created the security group resource for the flask ecs service

--- Terraform/terraform/Modules/ECS/security-group.tf
+++ Terraform/terraform/Modules/ECS/security-group.tf
@@ -23,3 +23,32 @@ resource "aws_security_group" "nextjs_service_sg" {
     Environment = var.environment
   }
 }
+
+
+# SECURITY GROUP FOR THE FLASK SERVICE
+# This security group allows traffic from the NextJS ECS service to the Flask ECS service
+resource "aws_security_group" "flask_service_sg" {
+  name        = "flask-ecs-sg"
+  description = "Allow traffic from Nextjs ECS to flask tasks"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [var.flask_alb_sg] # Allow only ALB SG to access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name        = "flask-ecs-sg"
+    Environment = var.environment
+  }
+}
