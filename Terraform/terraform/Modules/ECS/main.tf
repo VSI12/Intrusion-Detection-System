82db3ba64584a972936552216e27f399942ce8f5
Victor Iliya <VictorIliya15@gmail.com>
2025-04-05 10:08:47 +0100

    Created the task definition resource for the flassk bakcend

--- Terraform/terraform/Modules/ECS/main.tf
+++ Terraform/terraform/Modules/ECS/main.tf
@@ -134,3 +134,27 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+#ECS SERVICE FOR FLASK
+resource "aws_ecs_task_definition" "frontend" {
+  family                   = "backend-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "${var.flask_ecr_name}"
+      image     = "${var.flask_ecr}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 5000
+          hostPort      = 5000
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
