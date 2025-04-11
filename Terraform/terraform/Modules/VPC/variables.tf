464f23195c95e59391076222f1be09f6d90e3684
Victor Iliya <VictorIliya15@gmail.com>
2025-04-11 22:31:36 +0100

    redeclared the cidr block for the route table

--- Terraform/terraform/Modules/VPC/variables.tf
+++ Terraform/terraform/Modules/VPC/variables.tf
@@ -32,3 +32,7 @@ variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
 }
+variable "public_cidr" {
+  description = "CIDR block for public subnets"
+  type        = string
+}
