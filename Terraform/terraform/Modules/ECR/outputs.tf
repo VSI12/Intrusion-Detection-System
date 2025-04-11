085cab2dff90e94c51947e4f8e96ffb39f3f6bba
Victor Iliya <88778598+VSI12@users.noreply.github.com>
2025-04-11 11:52:37 +0100

    added description in the outputs module for the nextjs and flask ecrs

--- Terraform/terraform/Modules/ECR/outputs.tf
+++ Terraform/terraform/Modules/ECR/outputs.tf
@@ -1,9 +1,11 @@
 output "flask_repo_url" {
   value = aws_ecr_repository.flask_repo.repository_url
+  description = "The ECR URL for the flask ECR Repo"
 }
 
 output "nextjs_repo_url" {
   value = aws_ecr_repository.NextJS_ecr.repository_url
+  description = "The ECR URL for the NextJS ECR Repo"
 }
 
 output "flask_repo_arn" {
