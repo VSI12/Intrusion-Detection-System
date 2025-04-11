8245d542933fdd17fc3f1565766c491fe7f29a97
Victor Iliya <VictorIliya15@gmail.com>
2025-04-12 00:39:09 +0100

    Wrote the ccorrect coed for the dev output files

--- Terraform/terraform/Environments/Development/outputs.tf
+++ Terraform/terraform/Environments/Development/outputs.tf
@@ -1,12 +1,12 @@
 output "flask_repo_url" {
-  value = module.flask_app.repo_url
+  value = module.ecr.flask_repo_url
 }
 
 output "nextjs_repo_url" {
-  value = module.nextjs_app.repo_url
+  value = module.ecr.nextjs_repo_url
 }
 
 output "alb_dns_name" {
-  value = module.ids_alb_external.dns_name
+  value = module.alb.alb_dns_name
   
 }
