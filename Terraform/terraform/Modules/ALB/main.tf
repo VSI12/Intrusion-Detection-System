442645822eb82f28c2ffd4aa1ba91490d13f4789
Victor Iliya <VictorIliya15@gmail.com>
2025-04-19 09:08:00 +0100

    Back to HTTP

--- Terraform/terraform/Modules/ALB/main.tf
+++ Terraform/terraform/Modules/ALB/main.tf
@@ -31,7 +31,7 @@ resource "aws_lb_listener" "nextjs_fargate" {
 resource "aws_lb_target_group" "nextjs_fargate" {
   name        = var.nextjs_fargate_tg
   port        = 3000
-  protocol    = "TCP"
+  protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
 
