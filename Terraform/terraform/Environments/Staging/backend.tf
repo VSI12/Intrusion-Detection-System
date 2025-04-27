a80bc0c738d91a30b9e017f94cc26a9a03eacaa7
Victor Iliya <VictorIliya15@gmail.com>
2025-04-27 05:43:31 +0100

    Changed the s3nucket key for the terraform state file

--- Terraform/terraform/Environments/Staging/backend.tf
+++ Terraform/terraform/Environments/Staging/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ids-backend-bucket"
-    key            = "terraform/state/prod/terraform.tfstate"
+    key            = "terraform/state/staging/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "ids-backend-table-staging"
