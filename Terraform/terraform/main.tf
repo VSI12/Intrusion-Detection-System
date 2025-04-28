9e49af3fd3d7f79b27749fd3de60a60d04201b16
Victor Iliya <VictorIliya15@gmail.com>
2025-04-28 08:44:56 +0100

    removed remote s3 state

--- Terraform/terraform/main.tf
+++ Terraform/terraform/main.tf
@@ -1,10 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket         = "ids-backend-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "ids-backend-table-dev"
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
